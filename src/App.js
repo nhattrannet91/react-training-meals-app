0fbc0277539763d50a627d2062a75116f7b9a3be
Nhat Tran <nhat.tran@elca.vn>
2021-09-26 21:14:12 +0700

    Meals-App: Add Meals

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import {Fragment } from "react";
+import { Fragment } from "react";
+
 import Header from "./components/Layout/Header/Header";
+import Meals from "./components/Meals/Meals";
+
 function App() {
   return (
     <Fragment>
-      <Header/>
+      <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
