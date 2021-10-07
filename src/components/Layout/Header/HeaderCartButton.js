5349263fa104e56233adf8026cea1f9254a15116
Nhat Tran <nhat.tran@elca.vn>
2021-10-07 07:18:26 +0700

    Meals-App: Apply animation effect to HeaderCartButton

--- src/components/Layout/Header/HeaderCartButton.js
+++ src/components/Layout/Header/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import CartContext from "../../../store/cart-context";
 import CartIcon from "./CartIcon";
@@ -6,9 +6,25 @@ import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
   const cartCtx = useContext(CartContext);
+  const {items} = cartCtx;
+  const [isBump, setIsBump] = useState(false);
+
+  const btnClasses = `${classes.button} ${isBump ? classes.bump : ''}`;
+
+  useEffect(() => {
+    if(items.length === 0){
+      return; 
+    }
+
+    setIsBump(true);
+    const handler = setTimeout(() => setIsBump(false), 300);
+    return () => {
+      clearTimeout(handler)
+    };
+  }, [items]);
   
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
